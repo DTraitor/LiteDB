052beabcc3bfd845abb590a2e8cee65d300c700a
DTraitor <sehhd32@gmail.com>
2025-06-10 16:04:47 +0300

    Delete file in rebuild if something goes wrong

--- LiteDB/Engine/Services/RebuildService.cs
+++ LiteDB/Engine/Services/RebuildService.cs
@@ -51,39 +51,47 @@ namespace LiteDB.Engine
                 // open file reader and ready to import to new temp engine instance
                 reader.Open();
 
-                // open new engine to recive all data readed from FileReader
-                using (var engine = new LiteEngine(new EngineSettings
+                try
                 {
-                    Filename = tempFilename,
-                    Collation = options.Collation,
-                    Password = options.Password,
-                }))
-                {
-                    // copy all database to new Log file with NO checkpoint during all rebuild
-                    engine.Pragma(Pragmas.CHECKPOINT, 0);
-
-                    // rebuild all content from reader into new engine
-                    engine.RebuildContent(reader);
-
-                    // insert error report
-                    if (options.IncludeErrorReport && options.Errors.Count > 0)
+                    // open new engine to recive all data readed from FileReader
+                    using (var engine = new LiteEngine(new EngineSettings
+                           {
+                               Filename = tempFilename,
+                               Collation = options.Collation,
+                               Password = options.Password,
+                           }))
                     {
-                        var report = options.GetErrorReport();
+                        // copy all database to new Log file with NO checkpoint during all rebuild
+                        engine.Pragma(Pragmas.CHECKPOINT, 0);
 
-                        engine.Insert("_rebuild_errors", report, BsonAutoId.Int32);
+                        // rebuild all content from reader into new engine
+                        engine.RebuildContent(reader);
+
+                        // insert error report
+                        if (options.IncludeErrorReport && options.Errors.Count > 0)
+                        {
+                            var report = options.GetErrorReport();
+
+                            engine.Insert("_rebuild_errors", report, BsonAutoId.Int32);
+                        }
+
+                        // update pragmas
+                        var pragmas = reader.GetPragmas();
+
+                        engine.Pragma(Pragmas.CHECKPOINT, pragmas[Pragmas.CHECKPOINT]);
+                        engine.Pragma(Pragmas.TIMEOUT, pragmas[Pragmas.TIMEOUT]);
+                        engine.Pragma(Pragmas.LIMIT_SIZE, pragmas[Pragmas.LIMIT_SIZE]);
+                        engine.Pragma(Pragmas.UTC_DATE, pragmas[Pragmas.UTC_DATE]);
+                        engine.Pragma(Pragmas.USER_VERSION, pragmas[Pragmas.USER_VERSION]);
+
+                        // after rebuild, copy log bytes into data file
+                        engine.Checkpoint();
                     }
-
-                    // update pragmas
-                    var pragmas = reader.GetPragmas();
-
-                    engine.Pragma(Pragmas.CHECKPOINT, pragmas[Pragmas.CHECKPOINT]);
-                    engine.Pragma(Pragmas.TIMEOUT, pragmas[Pragmas.TIMEOUT]);
-                    engine.Pragma(Pragmas.LIMIT_SIZE, pragmas[Pragmas.LIMIT_SIZE]);
-                    engine.Pragma(Pragmas.UTC_DATE, pragmas[Pragmas.UTC_DATE]);
-                    engine.Pragma(Pragmas.USER_VERSION, pragmas[Pragmas.USER_VERSION]);
-
-                    // after rebuild, copy log bytes into data file
-                    engine.Checkpoint();
+                }
+                catch (Exception)
+                {
+                    File.Delete(tempFilename);
+                    throw;
                 }
             }
 
